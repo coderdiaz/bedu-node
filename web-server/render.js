32cc5a93c7385aa462bda6cc0966a6b6e374c9a1
AlanKimo <alan@iceberg9.com>
2019-03-23 11:55:18 -0600

    Exercise #1

--- web-server/render.js
+++ web-server/render.js
@@ -1,5 +1,19 @@
+const fs = require('fs');
+
 const renderView = (fileName) => {
   console.log(fileName);
+
+  switch(fileName) {
+  	case 'index':
+  		return fs.readFileSync('./views/index.html')
+  		break;
+  	case 'about':
+  		return fs.readFileSync('./views/about.html')
+  		break;
+  }
+
+
+  
 };
 
 module.exports = renderView;
